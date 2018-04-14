2c0d9979db8b903b0bd597490aba4bf4f709cb7f
Arry Tapiheroe <arry14@gmail.com>
2018-04-14 14:32:17 +0100

    finished API work and added examples, as per FCC example, to the HTML

--- api.js
+++ api.js
@@ -3,8 +3,33 @@ var moment = require('moment');
 module.exports = function(app) {
   
   app.get('/:query', function(req, res) {
-    var time = req.params.query;
-    res.send(moment(time));
+    var date = req.params.query;
+    var unix = null;
+    var natural = null;
+
+    // check if time is in unix
+    if (+date >= 0) {
+      unix = +date;
+      natural = unixToNatural(date);
+    }
+
+    // check if time is valid natural date
+    if (isNaN(+date) && moment(date, "MMMM, D YYYY").isValid()) {
+      unix = +naturalToUnix(date);
+      natural = unixToNatural(unix);
+    }
+
+    var dateObj = {"unix" : unix, "natural" : natural};
+    res.send(dateObj);
   });
-  
+
+  // function to convert natural to unix
+  function naturalToUnix(date) {
+    return moment(date, "MMMM, D YYYY").format("X");
+  }
+
+  // function to convert unix to natural
+  function unixToNatural(date) {
+    return moment.unix(date).format("MMMM, D YYYY");
+  }
 }
